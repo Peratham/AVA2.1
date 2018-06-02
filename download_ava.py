4c587fcbbff5bcb7c3a2b0af8b2435f81ba60fb8
Peratham Wiriyathammabhum <Peratham@users.noreply.github.com>
2018-06-01 20:53:39 -0400

    Update download_ava.py

--- download_ava.py
+++ download_ava.py
@@ -8,11 +8,11 @@ with open('./meta/ava_file_names_trainval_v2.1.txt','rb') as fp:
 vidlist = [x.strip() for x in vidlist]
 
 dummy_url = 'https://s3.amazonaws.com/ava-dataset/trainval/[file_name]'
+dst = os.path.join('.', 'Data', 'trainval')
+if not os.path.exists(dst):
+    os.makedirs(dst)
 print('[Info] Downloading trainval...')
 for i, vidname in enumerate(vidlist, 1):
-    dst = os.path.join('.', 'Data', 'trainval')
-    if not os.path.exists(dst):
-        os.makedirs(dst)
     vid_url = dummy_url.replace('[file_name]',vidname)
     print('[Info] Downloading {0}/{1}'.format(i,len(vidlist)))
     urllib.urlretrieve (vid_url, os.path.join(dst, "{}".format(vidname)))
@@ -23,11 +23,11 @@ with open('./meta/ava_file_names_test_v2.1.txt','rb') as fp:
 vidlist = [x.strip() for x in vidlist]
 
 dummy_url = 'https://s3.amazonaws.com/ava-dataset/test/[file_name]'
+dst = os.path.join('.', 'Data', 'test')
+if not os.path.exists(dst):
+    os.makedirs(dst)
 print('[Info] Downloading test...')
 for i, vidname in enumerate(vidlist, 1):
-    dst = os.path.join('.', 'Data', 'test')
-    if not os.path.exists(dst):
-        os.makedirs(dst)
     vid_url = dummy_url.replace('[file_name]',vidname)
     print('[Info] Downloading {0}/{1}'.format(i,len(vidlist)))
     urllib.urlretrieve (vid_url, os.path.join(dst, "{}".format(vidname)))
